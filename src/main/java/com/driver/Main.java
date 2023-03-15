9a146f377c9c4e947887ac0b14a6c0eca58cdb60
unknown <mjrajak47z@gmail.com>
2023-03-15 19:18:22 +0530

    Encapsulation Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+
+        RWOnly obj = new RWOnly();
+
+        // Set the name using set function
+        obj.setName("Kumar Devendra");
+
+        // get the name using get funtion
+        System.out.print(obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+    public String getName(){  // create getter function for get the private variable name
+        return name;
+    }
+
+    public void setName(String name){ //create setter function for set the private variable name
+        this.name = name;
+    }
 }
